34b6226b59bf6faa172f730743105f5b9c1b292b
Musaddiqur Rahman <mashrur749@gmail.com>
2020-04-06 00:38:00 -0400

    added eslint config & vscode settings

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,13 @@
-const   express = require('express'),
-        app = express(),
-        HTTP_PORT = process.env.PORT || 8080;
+const express = require('express');
+
+const app = express();
+const HTTP_PORT = process.env.PORT || 8080;
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.send('wut');
-})
-
+	res.send('something');
+});
 app.listen(HTTP_PORT, () => {
-    console.log("Express server starting on port: " + HTTP_PORT);
-})
+	console.log(`Express server starting on port: ${HTTP_PORT}`);
+});
