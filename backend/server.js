d953c531906f188557db41733d611eadfbba7462
Musaddiqur Rahman <mashrur749@gmail.com>
2020-04-08 05:12:39 -0400

    added object object linting settings

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,19 @@ const express = require('express');
 
 const app = express();
 const HTTP_PORT = process.env.PORT || 8080;
+const something = {
+	foo: 1,
+	fll: 23,
+	fll2w: 543,
+	bla: 2,
+	bla2: 2,
+	bla3: 2,
+};
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-	res.send('something');
+	res.send('Hello world');
 });
 app.listen(HTTP_PORT, () => {
 	console.log(`Express server starting on port: ${HTTP_PORT}`);
